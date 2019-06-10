5000b68318674df7cbf8a5b97d15cb3df6ec55fe
Robert Love <robertjohnlove@gmail.com>
2019-06-10 18:01:47 +1000

    Show/hide server vars

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,22 @@ layout: null
 {% include_relative jquery.min.js %}
 {% include_relative popper.min.js %}
 {% include_relative bootstrap.min.js %}
+
+$(function() {
+
+  // Show/hide server variables
+  if ($("#servers").length && $("#servers-variables").length) {
+    $("#servers").change(function() {
+      var target = $("#servers option:selected").attr("id") + "-variables";
+      console.log("Target", target);
+      $("#servers-variables").children().each(function() {
+        if ($(this).attr("id") == target) {
+          $(this).removeClass("d-none");
+        } else {
+          $(this).addClass("d-none");
+        }
+      });
+    });
+  }
+
+});
