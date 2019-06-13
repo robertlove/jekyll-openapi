6c7f50608f3ab001407bfc7bc3733b07c10ac310
Robert Love <robertjohnlove@gmail.com>
2019-06-13 13:30:46 +1000

    Servers WIP

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,29 +9,52 @@ layout: null
 $(function() {
 
   // Servers
-  if ($("#servers").length && $(".servers-variables").length) {
 
-    // Show/hide server variables
-    $("#servers-url").change(function() {
-      var target = $("#servers option:selected").attr("id") + "-variables";
-      console.log("Target", target);
-      $(".servers-variables").each(function() {
-        if ($(this).attr("id") == target) {
+  // If there is more than one server
+  if ($("#servers-2").length) {
+
+    // When the user changes servers
+    $("#servers").change(function() {
+
+      var server = $("#servers option:selected").attr("id");
+
+      // Show/hide the server description
+      var description = server + "-description";
+
+      $(".servers-description").each(function() {
+        if ($(this).attr("id") == description) {
           $(this).removeClass("d-none");
         } else {
           $(this).addClass("d-none");
         }
       });
+
+      // If there are server variables
+      if ($(".servers-variables").length) {
+
+        var variables = server + "-variables";
+
+        // Show/hide the server variables
+        $(".servers-variables").each(function() {
+          if ($(this).attr("id") == variables) {
+            $(this).removeClass("d-none");
+          } else {
+            $(this).addClass("d-none");
+          }
+        });
+
+      }
+
     });
 
-    var text = $("#servers-1-url-computed").val()
-    console.log(text);
-    text = text.replace("{username}", "demo");
-    console.log(text);
-    text = text.replace("{port}", "443");
-    console.log(text);
-    text = text.replace("{version}", "v3");
-    console.log(text);
+    // var text = $("#servers-1-url-computed").val()
+    // console.log(text);
+    // text = text.replace("{username}", "demo");
+    // console.log(text);
+    // text = text.replace("{port}", "443");
+    // console.log(text);
+    // text = text.replace("{version}", "v3");
+    // console.log(text);
 
   }
 
