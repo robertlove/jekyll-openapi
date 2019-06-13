45eb305302d283a789b9b0e5e1ea235fb66276b4
Robert Love <robertjohnlove@gmail.com>
2019-06-13 17:05:35 +1000

    Servers WIP

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,52 +10,68 @@ $(function() {
 
   // Servers
 
-  // If there is more than one server
-  if ($("#servers-2").length) {
+  // When the user changes servers
+  $("#servers").change(function() {
 
-    // When the user changes servers
-    $("#servers").change(function() {
+    var server = $("#servers option:selected").attr("id");
 
-      var server = $("#servers option:selected").attr("id");
+    // Show/hide the server description
+    var description = server + "-description";
 
-      // Show/hide the server description
-      var description = server + "-description";
+    $(".servers-description").each(function() {
+      if ($(this).attr("id") == description) {
+        $(this).removeClass("d-none");
+      } else {
+        $(this).addClass("d-none");
+      }
+    });
 
-      $(".servers-description").each(function() {
-        if ($(this).attr("id") == description) {
+    // If there are server variables
+    if ($(".servers-variables").length) {
+
+      var variables = server + "-variables";
+
+      // Show/hide the server variables
+      $(".servers-variables").each(function() {
+        if ($(this).attr("id") == variables) {
           $(this).removeClass("d-none");
         } else {
           $(this).addClass("d-none");
         }
       });
 
-      // If there are server variables
-      if ($(".servers-variables").length) {
+    }
 
-        var variables = server + "-variables";
+  });
 
-        // Show/hide the server variables
-        $(".servers-variables").each(function() {
-          if ($(this).attr("id") == variables) {
-            $(this).removeClass("d-none");
-          } else {
-            $(this).addClass("d-none");
-          }
-        });
+  // If there are server variables
+  if ($(".servers-variables").length) {
 
-      }
+    // For each set of server variables
+    $(".servers-variables").each(function() {
+
+      var variables = $(this);
+
+      // When the user changes a server variable
+      variables.children("input, select").bind("keyup change", function() {
+        var variable = $(this);
+        var name = variable.attr("name");
+        var value = variable.val();
+        console.log(name, value);
+      });
 
     });
-
-    // var text = $("#servers-1-url-computed").val()
-    // console.log(text);
-    // text = text.replace("{username}", "demo");
-    // console.log(text);
-    // text = text.replace("{port}", "443");
-    // console.log(text);
-    // text = text.replace("{version}", "v3");
-    // console.log(text);
 
   }
 
 });
+
+
+// var text = $("#servers-1-url-computed").val()
+// console.log(text);
+// text = text.replace("{username}", "demo");
+// console.log(text);
+// text = text.replace("{port}", "443");
+// console.log(text);
+// text = text.replace("{version}", "v3");
+// console.log(text);
