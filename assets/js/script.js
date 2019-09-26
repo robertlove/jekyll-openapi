8bae019b3a3647ae97dc17e438e926df3615aa55
Robert Love <robertjohnlove@gmail.com>
2019-09-26 15:46:15 +1000

    Schemas WIP

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,20 @@
 layout: null
 ---
 
+{% assign api = site.data.openapi %}
+
 {% include_relative jquery.min.js %}
 {% include_relative popper.min.js %}
 {% include_relative bootstrap.min.js %}
+
+{% if api.components.schemas %}
+  $(function() {
+    {% for schema in api.components.schemas %}
+      {% assign type = schema[1].type %}
+      {% assign properties = schema[1].properties %}
+      var {{ schema[0] }} = {% include json.html type=type properties=properties %};
+      console.log({{ schema[0] }});
+      $("[data-ref='{{ schema[0] }}']").html(JSON.stringify({{ schema[0] }}, null, '  '));
+    {% endfor %}
+  });
+{% endif %}
