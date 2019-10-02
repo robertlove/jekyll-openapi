13d1292fa9f2e71bb51b907aac5acdc38ed3fa16
Robert Love <robertjohnlove@gmail.com>
2019-10-02 13:22:36 +1000

    Whitespace control

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,28 +2,22 @@
 layout: null
 ---
 
-{% assign api = site.data.openapi %}
+{%- assign api = site.data.openapi -%}
 
-{% include_relative jquery.min.js %}
-{% include_relative popper.min.js %}
-{% include_relative bootstrap.min.js %}
-{% include_relative prism.min.js %}
+{%- include_relative jquery.min.js -%}
+{%- include_relative popper.min.js -%}
+{%- include_relative bootstrap.min.js -%}
+{%- include_relative prism.min.js -%}
 
 $(function() {
-
-  {% if api.components.schemas %}
-    {% for schema in api.components.schemas %}
-      {% assign type = schema[1].type %}
-      {% assign properties = schema[1].properties %}
-      var {{ schema[0] }} = {% include json.html type=type properties=properties %};
-      console.log({{ schema[0] }});
-      $("[data-ref='{{ schema[0] }}']").html(JSON.stringify({{ schema[0] }}, null, '  '));
-    {% endfor %}
-  {% endif %}
-
+  {%- if api.components.schemas -%}
+    {%- for schema in api.components.schemas -%}
+      {%- assign type = schema[1].type -%}
+      {%- assign properties = schema[1].properties -%}
+      var {{- schema[0] -}} = {%- include json.html type=type properties=properties -%};
+      console.log({{- schema[0] -}});
+      $("[data-ref='{{- schema[0] -}}']").html(JSON.stringify({{- schema[0] -}}, null, '  '));
+    {%- endfor -%}
+  {%- endif -%}
   Prism.highlightAll();
-
 });
-
-
-
