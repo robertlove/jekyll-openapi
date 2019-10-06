47f2549f8ba625146287784dc92bf6201fff6296
Robert Love <robertjohnlove@gmail.com>
2019-10-07 09:12:16 +1100

    req/res wip

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,6 @@
 layout: null
 ---
 
-{%- assign api = site.data.openapi -%}
-
 {%- include_relative jquery.min.js -%}
 {%- include_relative popper.min.js -%}
 {%- include_relative bootstrap.min.js -%}
-{%- include_relative prism.min.js -%}
-
-$(function() {
-  {%- if api.components.schemas -%}
-    {%- for schema in api.components.schemas -%}
-      var {{ schema[0] }} = {% include json_schema.html schema=schema %};
-      console.log({{- schema[0] -}});
-      $("[data-ref='{{- schema[0] -}}']").html(JSON.stringify({{- schema[0] -}}, null, '  '));
-    {%- endfor -%}
-  {%- endif -%}
-  Prism.highlightAll();
-});
