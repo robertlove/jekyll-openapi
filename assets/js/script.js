f6979767a4b375fd55c64502faeb3dbf26393d1f
Robert Love <robertjohnlove@gmail.com>
2019-10-08 12:56:46 +1100

    Format JSON

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,25 @@
 layout: null
 ---
 
-{%- include_relative jquery.min.js -%}
-{%- include_relative popper.min.js -%}
-{%- include_relative bootstrap.min.js -%}
+{% include_relative jquery.min.js %}
+{% include_relative popper.min.js %}
+{% include_relative bootstrap.min.js %}
+
+function isJSON(string) {
+  try {
+    JSON.parse(string);
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
+
+$(function() {
+  $(".language-json").each(function() {
+    var string = $(this).html();
+    if (isJSON(string)) {
+      var json = JSON.parse(string);
+      $(this).html(JSON.stringify(json, null, '  '));
+    }
+  });
+});
